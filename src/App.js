a6aead2b5757679af13fb1439d50f3f2847831da
als8921 <als8921@naver.com>
2022-08-27 17:34:42 +0900

    Effects_DependencyList

--- src/App.js
+++ src/App.js
@@ -4,16 +4,38 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   function onClick() {
     setValue((x) => x + 1);
   }
-  console.log("UPDATE");
-  function iRunOnlyOnce() {
-    console.log("ONCE");
+  function onChange(e) {
+    setKeyword(e.target.value);
   }
-  useEffect(iRunOnlyOnce, []);
+
+  useEffect(() => {
+    console.log("Run Only Once");
+  }, []);
+
+  useEffect(() => {
+    console.log("Run 'Keyword' Changes :", keyword);
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("Run 'Counter' Changes :", counter);
+  }, [counter]);
+
+  useEffect(() => {
+    console.log("Run 'Keyword, Counter' Changes :", keyword, counter);
+  }, [counter, keyword]);
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here.."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>ClickMe</button>
     </div>
