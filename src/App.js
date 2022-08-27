faf264c2eb5cf44de1dc5dd9dae386da3e5bb78d
als8921 <als8921@naver.com>
2022-08-27 16:48:31 +0900

    Effects_useEffect()

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  function onClick() {
+    setValue((x) => x + 1);
+  }
+  console.log("UPDATE");
+  function iRunOnlyOnce() {
+    console.log("ONCE");
+  }
+  useEffect(iRunOnlyOnce, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>ClickMe</button>
     </div>
   );
 }
